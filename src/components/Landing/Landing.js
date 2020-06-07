b951139e35423ef0e8f3e26f53ce889a8a57890c
John Watterson <john@breakthrewbrew.com>
2020-06-07 17:33:27 -0600

    :tada: Initial commit of Landing component

---
+++ src/components/Landing/Landing.js
@@ -0,0 +1,13 @@
+import React from "react";
+// import "./DestinationDetails.css";
+import PropTypes from "prop-types";
+
+const Landing = () => {
+  return(
+    <h1>
+    Testing
+    </h1>
+  )
+}
+
+export default Landing;
