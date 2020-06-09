076971b66bc1c9b0ac5ec6fad9552bd05cf9b81d
John Watterson <john@breakthrewbrew.com>
2020-06-09 13:16:21 -0600

    :bar_chart: Add testing for Landing component

---
+++ src/components/Landing/Landing.test.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Landing from "./Landing";
+import { MemoryRouter } from "react-router-dom";
+import { render, fireEvent } from "@testing-library/react";
+import "@testing-library/jest-dom/";
+import { Router } from "react-router";
+
+describe("Landing", () => {
+  it("as a user, I should see three main buttons on the Landing page.", () => {
+    const router = (
+      <MemoryRouter>
+        <Landing />
+      </MemoryRouter>
+    );
+
+    const { getByText, getAllByRole } = render(router);
+    const mostPopularButton = getByText("Popular Fish");
+    const menuButtons = getAllByRole("button");
+
+    expect(mostPopularButton).toBeInTheDocument();
+    expect(menuButtons).toHaveLength(3);
+  });
+});
